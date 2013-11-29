0cce26d7ad72f5e6532ba3e700d1dc94953996ac
cahgantcore <m.sena.luphdika@gmail.com>
2013-11-29 11:05:23 +0700

    Create linkteman.php

---
+++ linkteman.php
@@ -0,0 +1,12 @@
+
+<div style="float:left">
+	<form method="get" action="tugas3.php">
+		<input type="radio" name="uri" value="http://localhost/progin-itb/II3160-Tugas-18211011-18211053/index.php/search/korans">Harya-Divo<br>
+		<input type="radio" name="uri" value="http://localhost/progin-itb/pemrograman-integratif/artis.php">Fatoni-Parto<br>
+		<input type="radio" name="uri" value="http://localhost/progin-itb/pemrograman_integratif/informasi_xml.php?nim=all">Gilang-Antra<br>
+		<input type="submit">	
+	</form>
+</div>
+
+<br>
+<a href="index.php">Kembali ke Home</a>
