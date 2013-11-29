f95daaf1dd3ac425ba114ab9d0bd3fd6d7a00408
cahgantcore <m.sena.luphdika@gmail.com>
2013-11-29 15:19:19 +0700

    Update linkteman.php

--- linkteman.php
+++ linkteman.php
@@ -1,9 +1,8 @@
-
 <div style="float:left">
 	<form method="get" action="tugas3.php">
-		<input type="radio" name="uri" value="http://localhost/progin-itb/II3160-Tugas-18211011-18211053/index.php/search/korans">Harya-Divo<br>
-		<input type="radio" name="uri" value="http://localhost/progin-itb/pemrograman-integratif/artis.php">Fatoni-Parto<br>
-		<input type="radio" name="uri" value="http://localhost/progin-itb/pemrograman_integratif/informasi_xml.php?nim=all">Gilang-Antra<br>
+		<input type="radio" name="uri" value="http://localhost/II3160-Tugas-18211011-18211053/index.php/search/korans">Harya-Divo<br>
+		<input type="radio" name="uri" value="http://localhost/pemrograman-integratif/artis.php">Fatoni-Parto<br>
+		<input type="radio" name="uri" value="http://localhost/pemrograman_integratif/informasi_xml.php?nim=all">Gilang-Antra<br>
 		<input type="submit">	
 	</form>
 </div>
