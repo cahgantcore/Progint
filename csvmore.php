6b680f6891cba8c0b4c25810ac54ad88216d168b
cahgantcore <m.sena.luphdika@gmail.com>
2013-11-29 15:00:38 +0700

    Update csvmore.php

--- csvmore.php
+++ csvmore.php
@@ -1,49 +1,49 @@
 <?php
-	if (empty($_GET)) {
-    // no data passed by get
-	} else {
-		$kriteria = $_GET['kriteria']; $input = $_GET['input'];
+	$input = $_GET['tag'];
+	$nilai = $_GET['value'];
+	$xml=simplexml_load_file("data/csvtoxml.xml");
+	header("content-type: text/xml");
+	switch($input) {
+	                case null:
+	                        echo "kosong";
+	                        break;
+	                case 'all':
+	                        $xmlPath = "data/csvtoxml.xml";
+	                    $doc = new DOMDocument(); 
+	                    $doc->load($xmlPath); 
+	                    $xml = $doc; 
+	                        print $xml->saveXML();
+                      break;
+	                  default:
+	                        $xmlPath = "data/csvtoxml.xml";
+	                    $domDocument = new DOMDocument(); 
+	                    $domDocument->load($xmlPath); 
+
+	                        $doc = new DOMDocument('1.0','UTF-8');
+	                    $doc->formatOutput = true;
+
+	                    $tag = $input;
+	                    $value = $nilai;
+	                    $root = $doc->createElement('daftar_majalah');
+	                    $root = $doc->appendChild($root);
+	                    for ($i=0; $i<$domDocument->getElementsByTagName($tag)->length; $i++)
+	                    {
+	                        $curr = $domDocument->getElementsByTagName($tag)->item($i);  
+	                        if ($curr->nodeValue == $value)
+	                        {
+	                            $mjl = $doc->createElement('majalah');
+	                            $mjl = $root->appendChild($mjl);
+	                            $judul = $doc->createElement('Judul');
+	                            $judul = $mjl->appendChild($judul);
+	                            $text = $doc->createTextNode($domDocument->getElementsByTagName('Judul')->item($i)->nodeValue);
+	                            $text = $judul->appendChild($text);
+	                            $hal = $doc->createElement('Halaman');
+	                            $hal = $mjl->appendChild($hal);
+	                            $text = $doc->createTextNode($domDocument->getElementsByTagName('Halaman')->item($i)->nodeValue);
+	                            $text = $hal->appendChild($text);
+	                        }
+	                    }
+	                        $xml = $doc;
+	                        print $xml->saveXML();
 	}
 ?>
-	<div>
-		<textarea cols="50" rows="30" style="float:left">
-			<?php
-				echo "\n"; 
-				echo file_get_contents("./data/csvtoxml.xml"); 
-			?>
-		</textarea>
-		<textarea cols="50" rows="30" style="float:left">
-			<?php
-				if (empty($_GET)) {
-			    // no data passed by get
-				} else {
-					echo "\n";
-
-					$xml = simplexml_load_file('./data/csvtoxml.xml');
-
-					$path = '//majalah/'.$kriteria.'[.='.'"'.$input.'"'.']/parent::*';
-
-					$nodes = $xml->xpath($path);
-
-					foreach($nodes as $node)
-					{
-					    foreach($node as $name => $prop) {
-					        printf("%s: %s\n", $name, $prop);
-					    }
-					    echo "\n";
-					}
-				}
-			?>
-		</textarea>
-		<br>
-		<form name="query" action="csvmore.php" method="get">
-			<select name="kriteria">
-				<option value="Judul">Judul</option>
-				<option value="Halaman">Halaman</option>
-			</select>
-			<input type="text" name="input">
-			<input type="submit" value="Submit">
-		</form>
-	</div>
-	
-	<a href="index.php">Kembali ke Home</a>
